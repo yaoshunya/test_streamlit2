70e9811ab274a000499eff4e668dc6581f2f862d
yaonyan <newton800pola@gmail.com>
2022-09-06 15:11:27 +0900

    first commit

---
+++ src/main.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import yaml
+from func1 import func
+import os
+
+st.write(os.listdir())
+st.write(os.listdir('..'))
+st.write(os.listdir('../data/'))
+
+
+with open('data/user_info.yml') as file:
+    config = yaml.load(file, Loader=yaml.SafeLoader)
+
+def main():
+    func()
+    st.write('Hello world')
+    st.write(config)
+
+main()
